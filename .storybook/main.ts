8c54c9e1e98ad28958aff9f950cc6c46e88d3629
haesoo-y <haesoo9410@gmail.com>
2023-08-20 22:28:54 +0900

    :wrench: Storybook 설정 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,38 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+// @ts-nocheck
+
+import tsPath from 'tsconfig-paths-webpack-plugin';
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const TsconfigPathsPlugin = tsPath;
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/nextjs",
+    name: '@storybook/nextjs',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        plugins: [
+          ...(config.resolve.plugins || []),
+          new TsconfigPathsPlugin({
+            extensions: config.resolve.extensions,
+          }),
+        ],
+      },
+    };
   },
 };
 export default config;
