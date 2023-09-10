8a61768c8365092c3bc486bce707b6af1bf5982d
haesoo-y <haesoo9410@gmail.com>
2023-09-10 15:46:36 +0900

    fix: 충돌이슈

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import 'src/styles/globals.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Link Your Link</title>
+        <meta name="description" content="link your link" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
