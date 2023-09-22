353d937a46c3f765af1c4682f74f646aa5a42b8e
heyroocoke <heyroocoke@gmail.com>
2023-09-22 22:53:28 +0900

    🔥 컴포넌트 내 비지니스 로직 제거

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,24 +1,16 @@
-import { useState } from 'react';
 import S from './Checkbox.styled';
 
 import type { CheckboxProps } from './types';
 
 const Checkbox = ({ shapes, id, label, checked, disabled = false, ...args }: CheckboxProps) => {
-  const [isChecked, setIsChecked] = useState<boolean>(checked);
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked(e.target.checked);
-  };
-
   return (
     <S.CheckboxContainer>
       <S.Checkbox
         type="checkbox"
         shapes={shapes}
         id={id}
-        checked={isChecked}
+        checked={checked}
         disabled={disabled}
-        onChange={onChange}
         {...args}
       />
       {label && <label htmlFor={id}>{label}</label>}
