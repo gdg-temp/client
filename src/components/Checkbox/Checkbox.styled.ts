1f3db1ff826d38e0a1d2152faf282cc4a1421c36
heyroocoke <heyroocoke@gmail.com>
2023-09-22 21:25:53 +0900

    💄 체크박스 스타일 적용

--- src/components/Checkbox/Checkbox.styled.ts
+++ src/components/Checkbox/Checkbox.styled.ts
@@ -2,6 +2,16 @@ import styled from 'styled-components';
 
 import type { CheckboxProps } from './types';
 
+const CHECKBOX_SHAPES: Record<CheckboxProps['shapes'], string> = {
+  circle: "#cccfd3 url('/icons/Check.svg') 50% 50% no-repeat",
+  simple: "url('/icons/Check_simple.svg') 50% 50% no-repeat",
+};
+
+const CHECKBOX_SHAPES_CHECKED: Record<CheckboxProps['shapes'], string> = {
+  circle: "#5c42ff url('/icons/Check.svg') 50% 50% no-repeat",
+  simple: "url('/icons/Check_simple_checked.svg') 50% 50% no-repeat",
+};
+
 const CheckboxContainer = styled.div`
   display: flex;
   align-items: center;
@@ -11,10 +21,19 @@ const CheckboxContainer = styled.div`
   }
 `;
 
-const Checkbox = styled.input<CheckboxProps>`
+const Checkbox = styled.input.attrs({
+  type: 'checkbox',
+})<CheckboxProps>`
+  appearance: none;
   width: 24px;
   height: 24px;
   cursor: pointer;
+  border-radius: 50%;
+  background: ${({ shapes }) => CHECKBOX_SHAPES[shapes]};
+
+  &:checked {
+    background: ${({ shapes }) => CHECKBOX_SHAPES_CHECKED[shapes]};
+  }
 `;
 
 export default { CheckboxContainer, Checkbox };
