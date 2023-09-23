58784bbed07783666a47f0ecc48200a75bd815fd
heyroocoke <heyroocoke@gmail.com>
2023-09-23 16:48:57 +0900

    ✅ 스토리 내 onClick 속성 추가

--- src/components/Chip/Chip.stories.ts
+++ src/components/Chip/Chip.stories.ts
@@ -1,4 +1,5 @@
 import Chip from './index';
+import { action } from '@storybook/addon-actions';
 
 import type { StoryObj, Meta } from '@storybook/react';
 
@@ -12,6 +13,9 @@ const meta: Meta<typeof Chip> = {
     value: {
       description: '칩 버튼의 value값을 정합니다.',
     },
+    onClick: {
+      description: '클릭 시 실행 될 함수를 넣습니다.',
+    },
   },
 };
 export default meta;
@@ -21,6 +25,7 @@ export const Default: Story = {
   args: {
     isClicked: false,
     value: 'MBTI',
+    onClick: () => action('onClick Triggered')('test'),
   },
 };
 
