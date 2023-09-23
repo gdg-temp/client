715ffb15af19113df4d40fd0d4897c9522f657a4
heyroocoke <heyroocoke@gmail.com>
2023-09-23 15:53:59 +0900

    💄 Disabled 스타일 추가

--- src/components/Chip/Chip.styled.ts
+++ src/components/Chip/Chip.styled.ts
@@ -2,29 +2,16 @@ import styled from 'styled-components';
 
 import type { ChipProps } from './types';
 
-// const BACKGROUND_COLOR: Record<ChipProps['clicked'], string> = {
-//   default: 'transparent',
-//   clicked: '#5C42FF',
-// };
-
-// const BORDER: Record<ChipProps['clicked'], string> = {
-//   default: '1px solid #6E6F73',
-//   clicked: 'none',
-// };
-
 const Chip = styled.button<ChipProps>`
-  appearance: none;
   width: 66px;
   height: 40px;
   font-size: 14px;
-  border: ${({ isClicked }) => (isClicked ? 'none' : '1px solid #6E6F73')};
+  border: ${({ disabled, isClicked }) =>
+    disabled === false && isClicked ? 'none' : '1px solid #6E6F73'};
   border-radius: 33px;
-  background-color: ${({ isClicked }) => (isClicked ? '#5C42FF' : 'transparent')};
+  background-color: ${({ disabled, isClicked }) =>
+    disabled === false && isClicked ? '#5C42FF' : 'transparent'};
   color: white;
-  /* & :checked {
-    border: none;
-    background-color: #5c42ff;
-  } */
 `;
 
 export default { Chip };
