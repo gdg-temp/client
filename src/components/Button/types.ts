4fb82d980df1c94d37ab297ef2682d85b77a34de
heyroocoke <heyroocoke@gmail.com>
2023-09-26 16:47:24 +0900

    💄 버튼 컴포넌트 스타일 추가

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -2,7 +2,7 @@ import type { ReactNode, ButtonHTMLAttributes } from 'react';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
-  color: 'black' | 'blue' | 'red' | 'green' | 'white';
+  color: 'primary' | 'secondary';
   disabled?: boolean;
-  size: 'large' | 'medium' | 'small';
+  size: 'large' | 'medium' | 'small' | 'plus';
 }
