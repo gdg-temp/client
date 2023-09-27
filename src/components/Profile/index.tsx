b2c4e618268d6aa9c2b6f10ee2f21d67bc1d312c
haesoo-y <haesoo9410@gmail.com>
2023-09-27 23:07:59 +0900

    :sparkles: 프로필 컴포넌트 추가

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,22 @@
+import S from './Profile.styled';
+
+import type { ProfileProps } from './types';
+
+const Profile = ({ size = 'small', cameraBtn = false, src, onClick }: ProfileProps) => {
+  return (
+    <S.ProfileWrapper size={size}>
+      {src ? (
+        <S.Image src={src} />
+      ) : (
+        <S.DefaultImage size={size} src={'icons/default_profile.svg'} />
+      )}
+      {cameraBtn && (
+        <S.Badge size={size} onClick={onClick}>
+          <S.Image src={'icons/camera.svg'} />
+        </S.Badge>
+      )}
+    </S.ProfileWrapper>
+  );
+};
+
+export default Profile;
