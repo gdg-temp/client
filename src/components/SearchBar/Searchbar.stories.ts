acd327b1cae6321da4969f733fdcca9344a50381
hyunjin-C <zn9852@naver.com>
2023-09-30 13:01:57 +0900

    :recycle: 스토리북 description 삭제 및 Props 타입에 tsdoc 작성

--- src/components/SearchBar/Searchbar.stories.ts
+++ src/components/SearchBar/Searchbar.stories.ts
@@ -6,14 +6,6 @@ import type { StoryObj, Meta } from '@storybook/react';
 const meta: Meta<typeof SearchBar> = {
   title: 'components/SearchBar',
   component: SearchBar,
-  argTypes: {
-    onClick: {
-      description: '검색 또는 삭제 아이콘을 클릭했을 때 실행될 함수입니다.',
-    },
-    isWrite: {
-      description: '사용자가 내용 입력 시 나타나는 Delete 버튼입니다.',
-    },
-  },
 };
 
 export default meta;
