a02df0d21a3a3fb385766a0cc5a374c5e1b7d487
haesoo-y <haesoo9410@gmail.com>
2023-10-06 22:21:02 +0900

    :recycle: 모달 업데이트 & 버튼 수정하여 반영

--- src/components/Button/Button.styled.ts
+++ src/components/Button/Button.styled.ts
@@ -28,25 +28,23 @@ const FONT_COLOR: Record<string, string> = {
 };
 
 const BUTTON_WIDTH: Record<ButtonProps['size'], string> = {
-  large: '319px',
-  medium: '197px',
-  small: '95px',
-  plus: '74px',
+  large: '100%',
+  medium: '100%',
+  small: '74px',
 };
 
 const BUTTON_HEIGHT: Record<ButtonProps['size'], string> = {
   large: '54px',
-  medium: '54px',
-  small: '48px',
-  plus: '36px',
+  medium: '48px',
+  small: '36px',
 };
 
 const Button = styled.button<ButtonProps>`
   width: ${({ size }) => BUTTON_WIDTH[size]};
   height: ${({ size }) => BUTTON_HEIGHT[size]};
   border: ${({ disabled, color }) => (disabled ? 'none' : BUTTON_BORDER[color])};
-  border-radius: ${({ size }) => (size === 'plus' ? '40px' : '8px')};
-  font-size: ${({ size }) => (size === 'plus' ? '14px' : '16px')};
+  border-radius: ${({ size }) => (size === 'small' ? '40px' : '8px')};
+  font-size: ${({ size }) => (size === 'small' ? '14px' : '16px')};
   background-color: ${({ disabled, color }) => (disabled ? '#6e6f73' : BACKGROUND_COLOR[color])};
   color: ${({ disabled, color }) => (disabled ? '#A2A5AA' : FONT_COLOR[color])};
   &:hover {
