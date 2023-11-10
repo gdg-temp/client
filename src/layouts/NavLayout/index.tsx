4ab1bb3fe9c9375734c0f47cf0320b69dffcbf23
hyunjin-C <zn9852@naver.com>
2023-11-11 04:17:13 +0900

    :sparkles: 페이지에 따른 네비게이션바 레이아웃 설정

--- src/layouts/NavLayout/index.tsx
+++ src/layouts/NavLayout/index.tsx
@@ -3,7 +3,7 @@ import { userAtom } from '@stores';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
-import { ReactElement, useState } from 'react';
+import { ReactElement, useState, useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 import { getCards, getCollection } from '@api';
 import { KEY } from '@static';
@@ -27,14 +27,101 @@ const NavLayout = ({ children }: { children: ReactElement }) => {
     queryFn: () => getCollection(),
   });
 
-  // 페이지에 따른 네비게이션바 설정 필요
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    setisSidebarOpen(false);
+  }, [router.pathname]);
+
+  if (pathname === '/cards') {
+    return (
+      <>
+        <NavBar
+          leadingButton="menu"
+          trailingButton="add"
+          onClickLeft={() => setisSidebarOpen(!isSidebarOpen)}
+          onClickRight={() => router.push('/generation')}
+        />
+        {isSidebarOpen &&
+          (userState.name ? (
+            <SideBar
+              isLogined={true}
+              name={userState.name}
+              loginIcon={userState.oauthServerType}
+              myCardCnt={cardsData.data?.length}
+              collectCardCnt={collectionData.data?.length}
+              onClose={() => setisSidebarOpen(!isSidebarOpen)}
+              onClickLogout={() => {}}
+            />
+          ) : (
+            <SideBar
+              isLogined={false}
+              onClickLogin={() => router.push('/login')}
+              onClose={() => setisSidebarOpen(!isSidebarOpen)}
+            />
+          ))}
+        <ContentLayout>{children}</ContentLayout>
+      </>
+    );
+  } else if (pathname === '/generation') {
+    return (
+      <>
+        <NavBar
+          leadingButton="back"
+          onClickLeft={() => {
+            router.back();
+          }}
+        />
+        <ContentLayout>{children}</ContentLayout>
+      </>
+    );
+  } else if (pathname === '/cards/[id]') {
+    return (
+      <>
+        <NavBar
+          leadingButton="back"
+          trailingButton="flip"
+          onClickLeft={() => {
+            router.back();
+          }}
+          onClickRight={() => {}}
+        />
+        <ContentLayout>{children}</ContentLayout>
+      </>
+    );
+  } else if (pathname === '/cards/[id]/edit') {
+    return (
+      <>
+        <NavBar
+          title="명함 수정하기"
+          leadingButton="back"
+          onClickLeft={() => {
+            router.back();
+          }}
+        />
+        <ContentLayout>{children}</ContentLayout>
+      </>
+    );
+  } else if (pathname === '/settings') {
+    return (
+      <>
+        <NavBar
+          title="환경설정"
+          leadingButton="back"
+          onClickLeft={() => {
+            router.back();
+          }}
+        />
+        <ContentLayout>{children}</ContentLayout>
+      </>
+    );
+  }
   return (
     <>
       <NavBar
         leadingButton="menu"
-        trailingButton="add"
+        showSearchBar={true}
         onClickLeft={() => setisSidebarOpen(!isSidebarOpen)}
-        onClickRight={() => router.push('/generation')}
       />
       {isSidebarOpen &&
         (userState.name ? (
--- src/pages/cards/[id]/edit/index.tsx
+++ src/pages/cards/[id]/edit/index.tsx
@@ -1,8 +1,10 @@
-import { getServerSideUserProps } from '@utils';
+import { getNavLayout, getServerSideUserProps } from '@utils';
 
 const CardEditPage = () => {
   return <div>수정페이지</div>;
 };
 
 export default CardEditPage;
+
+CardEditPage.getLayout = getNavLayout;
 export const getServerSideProps = getServerSideUserProps;
