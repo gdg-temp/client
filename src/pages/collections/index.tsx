7fcacca264c86710eca1ae6498cf33165100924d
hyunjin-C <zn9852@naver.com>
2023-11-26 16:05:16 +0900

    :sparkles: Collections 페이지 생성

--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -1,17 +1,66 @@
-import Head from 'next/head';
-import { getNavLayout, getServerSideUserProps } from '@utils';
+import { useState, useEffect, ChangeEvent } from 'react';
+import styled from 'styled-components';
+import { getServerSideUserProps } from '@utils';
+import { useQuery } from '@tanstack/react-query';
+import { KEY } from '@static';
+import { getCollection } from '@api';
+import { Loading, Card } from '@components';
+import Link from 'next/link';
+import { Collection } from '@types';
+import { NavLayout } from '@layouts';
+
+const CollectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  max-height: 100vh;
+`;
 
 export default function CollectionsPage() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: [KEY.COLLECTION],
+    queryFn: getCollection,
+  });
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState<Collection[]>([]);
+
+  const handleSearchIconClick = () => {
+    if (searchResults.length > 0) {
+      console.log('검색 결과: ', searchResults);
+    } else {
+      console.log('찾으시는 명함이 존재하지 않습니다.');
+    }
+  };
+
   return (
     <>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-      <div>보관함 페이지</div>
+      {isError ? (
+        <div>ERROR</div>
+      ) : isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <NavLayout onClickSearch={handleSearchIconClick} />
+          <CollectionContainer>
+            {data.map((card) => {
+              return (
+                <Link href={`/cards/[id]`} as={`/cards/${card.cardId}`} key={card.encodeId}>
+                  <Card
+                    key={card.encodeId}
+                    name={card.name}
+                    email={card.email}
+                    styleTemplate={card.styleTemplate}
+                    designTemplate={card.designTemplate}
+                  />
+                </Link>
+              );
+            })}
+          </CollectionContainer>
+        </>
+      )}
     </>
   );
 }
-
-CollectionsPage.getLayout = getNavLayout;
 
 export const getServerSideProps = getServerSideUserProps;
