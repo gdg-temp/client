8fecd229ad59f0ebc330bd6290013b76f0758538
장은수 <107452022+heyroocoke@users.noreply.github.com>
2023-12-09 21:23:50 +0900

    ✨ 단일 카드 페이지 개발 (#72)
    * 🍱 중복된 아이콘 수정
    
    * 📦 카드 플립 라이브러리 추가
    
    * 💄 버튼 폰트 굵기 수정
    
    * ✅ 카드 스토리 프로필 추가
    
    * ✨ 카드삭제 요청 추가
    
    * 🏷️ 카드 삭제 요청 타입 추가
    
    * ✅ 목업 데이터 항목 수정
    
    * ✨ 단일 카드 페이지 개발
    
    * 💄 단일 카드 페이지 스타일 추가
    
    * 💄 단일 카드페이지 스타일파일 이동

--- src/templates/card/Card.styled.ts
+++ src/templates/card/Card.styled.ts
@@ -1,1 +1,105 @@
 import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 22px;
+`;
+const ButtonWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 10px;
+  position: relative;
+`;
+const LinkWrapper = styled.div`
+  width: 319px;
+  height: 284px;
+  border-radius: 8px;
+  padding: 20px 16px 72px;
+  background-color: ${({ theme }) => theme.colorGray.blueGray700};
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+const LinkItem = styled.a`
+  width: 100%;
+  height: 32px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const CardDetailBack = styled.div`
+  width: 319px;
+  height: 472px;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colorGray.blueGray700};
+  /* padding: 0 20px; */
+`;
+const CardDetailIntro = styled.div`
+  width: 100%;
+  height: 120px;
+  padding: 32px 20px 20px;
+`;
+const CardDetailReasons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
+  margin-bottom: 64px;
+  padding: 0 20px 0;
+`;
+const CardDetailInfo = styled.div`
+  border-top: 1px solid ${({ theme }) => theme.colorGray.blueGray600};
+  padding: 24px 20px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+const CardDetailInfoItem = styled.div`
+  width: 100%;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const DeleteButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row-reverse;
+  font-size: 14px;
+  font-weight: 500;
+`;
+const DeleteButton = styled.button`
+  color: ${({ theme }) => theme.colorGray.blueGray400};
+`;
+
+export default {
+  Container,
+  ButtonWrapper,
+  CardWrapper,
+  LinkWrapper,
+  LinkItem,
+  CardDetailBack,
+  CardDetailIntro,
+  CardDetailReasons,
+  CardDetailInfo,
+  CardDetailInfoItem,
+  DeleteButtonWrapper,
+  DeleteButton,
+};
